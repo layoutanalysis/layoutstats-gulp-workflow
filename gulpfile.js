5cb30297f6bf9332072fdc370db83571408d6046
fbu <senor.wickie@gmail.com>
2018-01-25 17:44:29 +0100

    concatenate all cssstats json files for cssstats-diff (incomplete)

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,45 @@
 var gulp = require('gulp');
 var run = require('gulp-run');
 var cssLonghand = require('gulp-css-longhand');
-var colorguard = require('gulp-colorguard');
 var ext_replace = require('gulp-ext-replace');
+var jsonConcat = require('gulp-json-concat');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
+var concat = require('gulp-concat');
+
+
+
+gulp.task('clean:dist', function(){
+  return gulp.src('dist/*')
+        .pipe(vinylPaths(del))
+});
  
-gulp.task('default', function () {
-    return gulp.src('snapshot-css/*.css')
+gulp.task('cssstats',['clean:dist'], function () {
+     var pipeline = gulp.src('snapshot-css/*.css')
         .pipe(cssLonghand())
         .pipe(run('cssstats'))
         .pipe(ext_replace('.json'))
         .pipe(gulp.dest('dist'));
+     return pipeline;   
 });
+
+gulp.task('concatjson',['cssstats'], function(){
+         gulp.src('dist/*.json')
+        .pipe(jsonConcat('db.json',function(data){
+            var jsonFileNames = Object.keys(data);
+            var outJSON = jsonFileNames.map(function(fileName){
+                var cssStats = data[fileName];
+                
+                var cssProps = cssStats["declarations"]["properties"];
+                var cssPropKeys = Object.keys(cssProps);
+                var NonVendorProps = cssPropKeys.filter(function(prop){
+                  return prop.startsWith('-') === false;
+                });
+                
+            });
+            return new Buffer(JSON.stringify(outJSON));
+        }))
+        .pipe(gulp.dest('dist/'))
+});
+//'clean:dist','cssstats',
+gulp.task('default',['concatjson']);
